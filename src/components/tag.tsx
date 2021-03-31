dbfb80c93b0521f3e0c53c00bb22c0c56adcfe3f
Maurice Kleine <hey@mauricekleine.com>
2021-03-31 22:18:18 +0200

    fix: single line tags in some browsers

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -17,7 +17,7 @@ const Tag = ({ children, href }: Props) => (
             bg.dark,
             bg.darkHover,
             text.dark,
-            "font-sans mr-2 px-1.5 py-0.5 rounded-md text-xs underline uppercase whitespace-nowrap hover:bg-gray-100"
+            "font-sans inline-block mr-2 px-1.5 py-0.5 rounded-md text-xs underline uppercase whitespace-nowrap hover:bg-gray-100"
           )}
           href={href}
           rel="noopener noreferrer"
@@ -30,7 +30,7 @@ const Tag = ({ children, href }: Props) => (
           className={classNames(
             bg.dark,
             text.dark,
-            "font-sans mr-2 px-1.5 py-0.5 rounded-md text-xs uppercase whitespace-nowrap"
+            "font-sans inline-block mr-2 px-1.5 py-0.5 rounded-md text-xs uppercase whitespace-nowrap"
           )}
         >
           {children}

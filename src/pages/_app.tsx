ca750342718f83e2f63f11c306015e6312ae57cf
Maurice Kleine <hey@mauricekleine.com>
2021-08-09 14:49:32 +0200

    feat: move font imports to _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import Head from "next/head";
 import Nav from "../components/nav";
 
 import "tailwindcss/tailwind.css";
-import "../css/styles.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <div className="antialiased bg-primary-900 dark:bg-black">
@@ -21,13 +20,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
 
     <Nav />
 
-    <div className="bg-primary-50 bottom-2 fixed left-2 right-2 rounded-lg top-16 dark:hidden sm:bottom-4 sm:left-4 sm:right-4"></div>
+    <div className="fixed rounded-lg bg-primary-50 bottom-2 left-2 right-2 top-16 dark:hidden sm:bottom-4 sm:left-4 sm:right-4"></div>
 
-    <main className="font-serif my-16 mx-auto p-2 relative text-primary-700 w-full dark:text-primary-200 sm:w-4/6 md:w-4/5 lg:w-3/5">
+    <main className="relative w-full p-2 mx-auto my-16 font-serif text-primary-700 dark:text-primary-200 sm:w-4/6 md:w-4/5 lg:w-3/5">
       <Component {...pageProps} />
     </main>
 
-    <div className="bg-primary-900 bottom-0 fixed left-0 h-2 right-0 z-50 dark:hidden sm:h-4" />
+    <div className="fixed bottom-0 left-0 right-0 z-50 h-2 bg-primary-900 dark:hidden sm:h-4" />
   </div>
 );
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link href="https://fonts.googleapis.com" rel="preconnect" />
+          <link
+            crossOrigin="anonymous"
+            href="https://fonts.gstatic.com"
+            rel="preconnect"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700;900&family=Merriweather:wght@400;700&display=swap"
+          />
+        </Head>
 
         <body className="bg-primary-900 dark:bg-black">
           <Main />
+
           <NextScript />
         </body>
       </Html>

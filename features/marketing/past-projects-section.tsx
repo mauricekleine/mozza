63d279159bb25ec26ecba3f7db722cc69d3ca445
Maurice Kleine <hey@mauricekleine.com>
2022-11-11 22:27:37 +0100

    chore: logo cloud improvements

--- features/marketing/past-projects-section.tsx
+++ features/marketing/past-projects-section.tsx
@@ -33,19 +33,19 @@ export function PastProjectsSection() {
         <Section>
           <Grid
             alignItems="center"
-            columns={{ base: 2, md: 5 }}
+            columns={{ base: 2, sm: 3, lg: 5 }}
             gap={8}
             justifyItems="center"
             rows={{ base: "none", md: 2 }}
           >
             <LogoLink href="https://ubutler.nl/" logo={UButlerLogo} />
 
+            <LogoLink href="https://quest.app/" logo={QuestLogo} />
+
             <LogoLink
               href="https://productboard.com/"
               logo={ProductboardLogo}
             />
-
-            <LogoLink href="https://quest.app/" logo={QuestLogo} />
 
             <LogoLink href="https://aeon.co/" logo={AeonLogo} />
 

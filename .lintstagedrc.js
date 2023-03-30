5b413c0ea4434a594e5fb43e05ffb12bbd4249b7
Maurice Kleine <hey@mauricekleine.com>
2023-03-30 09:41:45 +0200

    chore: sort all package.json files and locally install taze

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --fix"],
   "*.{js,json,jsx,md,mdx,ts,tsx}": ["prettier --write"],
-  "*package.json": ["sort-package-json"],
+  "**/package.json": ["sort-package-json"],
 };

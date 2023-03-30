29b907b8a42f6b7ffa80ee251b502b05ba6d292f
Maurice Kleine <hey@mauricekleine.com>
2023-03-30 10:03:20 +0200

    fix(mauricekleine.com): tailwind config

--- apps/mauricekleine.com/tailwind.config.ts
+++ apps/mauricekleine.com/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import fontFamily from "tailwindcss/defaultTheme";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: [
